a33f91f890683fda9f95b9626fba02c1b7b455b6
WilliamADSR <Williamschaafsma@gmail.com>
2022-01-14 12:00:24 +0100

    This is my personal file
    I manipulated the integers to numeric and made sure the dichotomous variables are still doubles ( 1 0 )
    
    I also deleted the 172 missing observations on cholesterol
    
    made a start with descriptives to test
    
    did the first glm exploration

---
+++ William_Assignment2file.R
@@ -0,0 +1,78 @@
+#1. Required Packages (everyone can add whatever they need)
+library(readr)
+library(dbplyr)
+library(tidyverse)
+library(ggplot2)
+library(magrittr)
+library(lattice)
+
+#2. Loading the 'Heart Failure Prediction' data set
+{
+heart <- read_csv("heart.csv", 
+                  col_types = cols(Age = col_number(), 
+                  RestingBP = col_number(), Cholesterol = col_number(), 
+                  FastingBS = col_double(), RestingECG = col_character(), 
+                  MaxHR = col_number(), ExerciseAngina = col_character(),
+                  Oldpeak = col_number(), ST_Slope = col_character(),
+                  HeartDisease = col_double()))
+
+}
+
+#3. Inspection of the data set
+View(heart) 
+summary(heart)
+str(heart)
+head(heart)
+
+#4. Explanation of the variables
+
+#This data set holds 12 variables of which HeartDisease is our dichotomous outcome variable. A futher elaboration on the variables follows:
+#Variable 1: Age. It's a numeric variable of the patients' age expressed in years.
+#Variable 2: Sex. It's a categorical variable which holds the groups: F = female & M = male
+#Variable 3: ChestPaintype. It's a categorical variable which contains 4 different types of chest pains.
+          #TA: Typical Angina, a type of chest pain caused by reduced blood flow to the heart
+          #ATA: Atypical Angina, a type of chest pain that doesn’t meet the criteria for angina
+          #NAP: Non-Anginal Pain, a type of chest pain that is not caused by heart disease or a heart attack
+          #ASY: Asymptomatic, a type of chest pain that includes no chest pain but there is angina
+#Variable 4: RestingBP. It's a numeric variable representing patients' resting blood pressure (mmHg). The average blood pressure for an adult male is 120/80 mmHg. Average blood pressure increases when people get older. Furthermore, women's average blood pressure is mostly a little bit lower compared to males. A resting blood pressure > 140 is considered high and may cause heart failure.
+#Variable 5: Cholesterol. It's a numeric variable representing patients' cholesterol levels (mm / dl). A healthy adult has a cholesterol level lower than 200 mm / dl. High cholesterol levels (> 240) is unhealthy and therefore may cause heart failure.
+#Variable 6: FastingBS. It's a dichotomous variable representing patients' fasting blood sugar (mg / dl). Fasting blood sugar levels > 120 mg / dl are coded as a 1. Fasting blood sugar levels < 120 mg / dl are coded as a 0. High levels of blood sugar infer diabetes which may cause heart failure. 
+#Variable 7: RestingECG. It's a categorical variable which contains 3 different types of electric activity in the heart.
+          #Normal: The heart is beating in a regular sinus rhythm at a normal pace. No danger whatsoever 
+          #ST: Indicates an abnormality in the heartbeat. It is associated with increased cardiovascular risk.
+          #LVH: This activity in the heart represents valve problems which increasingly thickens the wall of the heart's main pumping chamber. It's the most common cause of high blood pressure which may cause heart failure.
+#Variable 8: MaxHR.
+#Variable 9: Exercise Angina.
+#Variable 10: Oldpeak
+#Variable 11: ST_slope
+#Variable 12: HeartDisease.
+
+#5. Excluding the NA's of cholesterol
+{
+Clean_Heart <- heart[-c(294:416, 422, 
+                        424:425, 428:431, 
+                        435:443, 536:537, 
+                        519, 515:516, 509, 
+                        493, 484:485, 480:482,
+                        478, 475:476, 471:473,
+                        467:468, 464:465,
+                        462, 456:460, 450:452,
+                        454, 447), ]
+}
+
+#6. Descriptives (start)
+Hist_age <- hist(Clean_Heart$Age)
+summary(Clean_heart$Age)
+#Age is distributed normally. The youngest observations is 28 years old and the oldest observation is 77 years old.
+
+Hist_chol <- hist(Clean_heart$Cholesterol)
+summary(Clean_heart$Cholesterol)
+#The Cholesterol variable has 172 observations of the number 0. This means there are 172 patients without data on cholesterol levels. Therefore these observations will be exluded from the dataset since a value of 0 on cholesterol levels is impossible. 
+
+
+#7. Building a model
+Model1 <- glm(HeartDisease ~ Cholesterol, data = Clean_heart, family = "binomial")
+summary(Model1)
+
+
+
